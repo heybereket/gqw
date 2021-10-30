300ea474dc7aa7e4c8574f660341641ea7c97ceb
Bereket Semagn <semagnbereket@gmail.com>
2021-10-30 19:12:35 -0400

    chore: add query user example

--- examples/basic/src/index.ts
+++ examples/basic/src/index.ts
@@ -3,6 +3,7 @@ import { GQW } from "../../../src/gqw";
 const client = new GQW(process.env.GITHUB_TOKEN);
 
 (async () => {
-  console.log(await client.queryEmail("semagnbereket@gmail.com"));
+  console.log(await client.queryUser("heybereket"));
+  console.log(await client.queryEmail("bereket@gmail.com"));
   console.log(await client.queryRepo("facebook/react"));
 })();
