ad2d5cd9c1bae18ff61752f566dfdc9155eca461
Bereket Semagn <semagnbereket@gmail.com>
2021-10-31 09:21:55 -0400

    fix formatting

--- src/gqw.ts
+++ src/gqw.ts
@@ -5,8 +5,7 @@ import fetch from "node-fetch";
 export class GQW {
   private readonly octokit: Octokit;
   public readonly queryUser = (user: string) => queryUser(this.octokit, user);
-  public readonly queryEmail = (email: string) =>
-    queryEmail(this.octokit, email);
+  public readonly queryEmail = (email: string) => queryEmail(this.octokit, email);
   public readonly queryRepo = (repo: string) => queryRepo(this.octokit, repo);
 
   constructor(token?: string) {
